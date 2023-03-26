b49ad9fcc6a536ea72fca9925b54a993697a12f2
VirLg <19virlg@gmail.com>
2023-03-26 20:07:20 +0300

    fetch

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import API from './js/servise-api'
+
+const Api = new API.DashFortnite();
+
+console.log(Api);
+
+
+const refForm = document.querySelector('.search-form')
+refForm.addEventListener('submit', handleForm)
+
+
+
+async function handleForm(evt) { 
+    evt.preventDefault()
+    const value = evt.target.elements.search.value
+const a = await Api.fetchApi()
+
+    console.log(a);
+
+}
---
+++ src/js/servise-api.js
@@ -0,0 +1,22 @@
+
+
+class DashFortnite {
+
+    // BASE_URL = 'https://fortnite-api.com/v1/playlists/',
+    
+    async fetchApi(params) {
+        
+        
+            return await fetch('https://fortnite-api.com/v1/playlists/');
+    
+       
+    }
+
+}
+
+
+
+
+
+
+export default {DashFortnite }
